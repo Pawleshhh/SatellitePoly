5a12681c340f460942bb7b04e98c8501e600d4cf
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-11-29 21:49:52 +0100

    Implement satellite service, part 2

--- Source/Top50Satellites.SatelliteService/SatelliteService.py
+++ Source/Top50Satellites.SatelliteService/SatelliteService.py
@@ -1,10 +1,23 @@
 import argparse
-from skyfield.api import load, wgs84
+import collections.abc
+from skyfield.api import load, wgs84, N, W
 
 # Parameters
 displayOutput = True
 output_path = ''
 args = None
+
+def check_if_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    else:
+        return False
+    
+def get_month_days(year, monthIndex):
+    leap = check_if_leap_year(year)
+    months = [31, 29 if leap else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+
+    return months[monthIndex - 1]
 
 def load_satellites_tle(tle_path):
     """ Load satellites from given location
@@ -29,18 +42,36 @@ def satellite_geographic_position(t, satellite):
     return [ wgs84.geographic_position_of(g) for g in geocentric ]
             
 def satellite_horizon_position(t, geo_position, satellite):
-    difference = satellite - geo_position
+    coords = wgs84.latlon(geo_position[0] * N, geo_position[1] * W)
+    difference = satellite - coords
     return [ difference.at(time).altaz() for time in t ]
 
 def get_time_interval(time, ts):
+
+    def check_time_part(time_parts, max_parts, result):
+        length = len(time_parts)
+        if length == 0:
+            return result
+        elif time_parts[0] < max_parts[0]:
+            result.append(time_parts[0])
+        else:
+            if length > 1:
+                time_parts[1] = time_parts[1] + 1
+            result.append(time_parts[0] - max_parts[0])
+
+        return check_time_part(time_parts[1:], max_parts[1:], result)
+
     [year, month, day, hour, minute, second, interval, count] = time
     t_start = ts.utc(year, month, day, hour, minute, second)
     t = t_start
     result = []
 
-    #for t in range(count):
-    #    result.append(t)
-    #    t = t.
+    for i in range(count):
+        result.append(t)
+        [second, minute, hour, day, month, year] = check_time_part([second + interval, minute, hour, day, month, year], [60, 60, 24, get_month_days(year, month), 12, 10000], [])
+        t = ts.utc(year, month, day, hour, minute, second)
+
+    return result
 
 def load_satellites(args):
     if args.source == 'database':
@@ -75,7 +106,7 @@ def parse_arguments():
                         help='specifies coordinates of the observator')
     parser.add_argument('--time', dest='time', action='store',
                         type=int, nargs=8,
-                        help='specifies time interval of calculations (year, month, day, hour, minute, second, interval in ms, count)')
+                        help='specifies time interval of calculations (year, month, day, hour, minute, second, interval in s, count)')
 
     parser.add_argument('--update', '-u', dest='update', action='store',
                         choices=['all', 'visual', 'weather'],
@@ -86,16 +117,48 @@ def parse_arguments():
 
     return parser.parse_args()
 
+def print_horizon_result(result):
+
+    def print_single_horizon(hor):
+        alt, az, dist = hor
+        print('Alt = ', alt, ' Az = ', az)
+        
+    for r in result:
+        for hor in r:
+            print_single_horizon(hor)
+
 def main():
     args = parse_arguments()
     
     ts = load.timescale()
 
     result = None
+    satellites = None
     if args.load is not None:
-        result = load_satellites()
-    elif args.position is not None:
-        result = None
+        satellites = load_satellites_tle(args.load)
+
+    if args.position is not None:
+        
+        if satellites is None:
+            print('Specify location of satellite data')
+        elif args.observator is None:
+            print('Specify location of observator')
+        else:
+            result = []
+            print('LENGTH', len(satellites))
+            i = 0
+            for sat in satellites:
+                print(i)
+                i = i + 1
+                if args.position == 'geo':
+                    result.append(satellite_geographic_position(get_time_interval(args.time, ts), sat))
+                else:
+                    result.append(satellite_horizon_position(get_time_interval(args.time, ts), args.observator, sat))
+            if args.show and result is not None:
+                print_horizon_result(result)
+    
+
+
 
 if __name__ == "__main__":
     main()
