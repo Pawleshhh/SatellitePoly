79a1f22648344d00dc90738c07fccccb8f60ccbf
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-12-04 20:06:54 +0100

    Reimplementation, part 1

--- Source/Top50Satellites.SatelliteService/SatelliteService.py
+++ Source/Top50Satellites.SatelliteService/SatelliteService.py
@@ -1,34 +1,46 @@
-from skyfield.api import load
-from modules.initializeArgs import parse_arguments
-from modules.printResult import print_horizon_result
-from modules.satellite import load_satellites_tle, satellite_geographic_position, satellite_horizon_position, get_time_interval
+from skyfield.api import load, wgs84, N, W
+from numpy.polynomial import Chebyshev
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+from modules.initializeArgs import parse_arguments, check_args, get_datetime_format
+from modules.satellite import load_satellites_tle, satellite_horizon_position
 
 def main():
     args = parse_arguments()
     
+    if check_args(args) == False:
+        return
+    
     ts = load.timescale()
 
-    result = None
-    satellites = None
-    if args.load is not None:
-        satellites = load_satellites_tle(args.load)
+    satellites = load_satellites_tle(args.load)
+    satellite = satellites[0]
 
-    if args.position is not None:
+    lat, lon, el = args.observator
+    observator = wgs84.latlon(lat * N, lon * W, el)
+    start = parse(args.start)
+    interval = args.interval
+    degrees = args.degrees
+    
+    date = start
+    altitudes = []
+    azimuths = []
+    seconds_axis = []
+    for second in range(interval):
+        time = ts.from_datetime(date)
+        alt, az, mag = satellite_horizon_position(time, observator, satellite)
+        altitudes.append(alt.degrees)
+        azimuths.append(az.degrees)
+        seconds_axis.append(second)
+        date = date + timedelta(seconds=1)
         
-        if satellites is None:
-            print('Specify location of satellite data')
-        elif args.observator is None:
-            print('Specify location of observator')
-        else:
-            result = []
-            for sat in satellites:
-                if args.position == 'geo':
-                    result.append(satellite_geographic_position(get_time_interval(args.time, ts), sat))
-                else:
-                    result.append(satellite_horizon_position(get_time_interval(args.time, ts), args.observator, sat))
-            
-            if args.show and result is not None:
-                print_horizon_result(result)
+    polyfit_alt = Chebyshev.fit(seconds_axis, altitudes, deg=degrees)
+    polyfit_az = Chebyshev.fit(seconds_axis, azimuths, deg=degrees)
+    
+    print("Altitude polynomial")
+    print(polyfit_alt)
+    print("Azimuth polynomial")
+    print(polyfit_az)
 
 if __name__ == "__main__":
     main()
