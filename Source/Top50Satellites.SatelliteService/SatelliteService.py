a3328addf0896e565df53d50f399bac69174282e
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-12-07 19:32:37 +0100

    Implement generating csv file as an output

--- Source/Top50Satellites.SatelliteService/SatelliteService.py
+++ Source/Top50Satellites.SatelliteService/SatelliteService.py
@@ -4,6 +4,45 @@ from modules.initializeArgs import parse_arguments, check_args
 from modules.satelliteutil import load_satellites_tle, find_satellite_by_id
 from modules.polynomials import get_polynomials_of_horizon_position
 
+def create_coords_row(name, coords):
+    row = name
+    for x in coords:
+        row = row + ', ' + str(x)
+    return row
+
+def write_polynomials(output, polynomials, satellite, observator, start, interval):
+    alt, az, el = polynomials
+
+    if output == './':
+        output = output + 'polynomials.csv'
+
+    maxLength = max(len(el), max(len(alt), len(az)))
+    if maxLength == 0:
+        return
+
+    sat_name_row = 'Satellite, ' + str(satellite.name)
+    observator_row = 'Observator coords, ' + str(observator)
+    time_row = 'Start date time, ' + str(start) + ', Interval in seconds, ' + str(interval)
+
+    headers_row = 'Coordinates'
+    for i in range(maxLength):
+        headers_row = headers_row + ', x' + str(i)
+
+    alt_row = create_coords_row("Altitude", alt)
+    az_row = create_coords_row("Azimuth", az)
+    el_row = create_coords_row("Elevation", el)
+
+    with open(output, 'w') as f:
+        for row in [sat_name_row, 
+                    observator_row, 
+                    time_row, 
+                    headers_row, 
+                    alt_row, 
+                    az_row, 
+                    el_row]:
+            f.write(row)
+            f.write('\n')
+
 def main():
     args = parse_arguments()
     
@@ -33,9 +72,7 @@ def main():
     
     alt, az, el = get_polynomials_of_horizon_position(ts, start, interval, observator, satellite, degrees)
     
-    print(str(alt.coef))
-    print(str(az.coef))
-    print(str(el.coef))
+    write_polynomials(args.output, [alt, az, el], satellite, observator, start, interval)
         
 if __name__ == "__main__":
     main()
