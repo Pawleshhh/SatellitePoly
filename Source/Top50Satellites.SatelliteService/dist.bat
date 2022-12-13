1c9cee2f73054f0b7cfd17ab151b2dd8d18df66e
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-12-13 19:50:59 +0100

    setupy.py

--- Source/Top50Satellites.SatelliteService/dist.bat
+++
@@ -1,6 +0,0 @@
-rd /s /q "./dist"
-py -m pip install --upgrade build
-py -m build
-py -m pip install --upgrade pip
-py -m pip install --force-reinstall ./dist/satelliteservice-0.0.1-py3-none-any.whl
-pause
